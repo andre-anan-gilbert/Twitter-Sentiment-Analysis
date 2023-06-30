a5364c3eb54c9e61c14c28c74361843fef31bbb3
David Hoffmann <adavidho@amazon.com>
2023-06-30 15:34:38 +0200

    Changed Logging Format for Python

--- src/spark-app/src/model.py
+++ src/spark-app/src/model.py
@@ -7,6 +7,9 @@ from pyspark.ml.evaluation import BinaryClassificationEvaluator, MulticlassClass
 from session import spark
 import logging
 
+# Set logging format
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', datefmt='%d/%m/%y %H:%M:%S')
+
 _MODEL_PATH = '/app/model/'
 
 
--- src/spark-app/src/session.py
+++ src/spark-app/src/session.py
@@ -1,6 +1,9 @@
 import logging
 from pyspark.sql import SparkSession
 
+# Set logging format
+logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', datefmt='%d/%m/%y %H:%M:%S')
+
 spark = SparkSession.builder.appName("Twitter Sentiment Analysis").getOrCreate()
 spark.sparkContext.setLogLevel('WARN')
 
